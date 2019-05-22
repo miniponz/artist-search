1664d602dbe75436b64cdb3c63bf2e066eb372fa
Cara Ponzini <ponzini@gmail.com>
2019-05-22 10:03:48 -0700

    artist search styled

--- src/components/Paging.js
+++ src/components/Paging.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './pagingStyles.css';
 
 function Paging({ currentPage, nextPage, previousPage, totalPages }) {
   return (
-    <>
-    {currentPage > 1 && <button onClick={previousPage}>Previous</button>}
-    <span>Page {currentPage} of {totalPages}</span>
-    {currentPage < totalPages && <button onClick={nextPage}>Next</button>}
-    </>
+    <div className={styles.paging}>
+      {currentPage > 1 && <button onClick={previousPage}>Previous</button>}
+      <span>Page {currentPage} of {totalPages}</span>
+      {currentPage < totalPages && <button onClick={nextPage}>Next</button>}
+    </div>
   );
 }
 
